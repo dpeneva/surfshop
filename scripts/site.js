68d6a080cbe9ff0ce9ac2f9d81a77f27c5a85819
dpeneva <daniela.p.peneva@gmail.com>
2019-08-10 00:18:26 +0300

    update navigation, form and products

--- scripts/site.js
+++ scripts/site.js
@@ -1,23 +1,23 @@
-// window.addEventListener('load', (event) => {
-//     document.getElementsByTagName('body')[0].classList.add('loaded');
-//  }); 
-
-let last_known_scroll_position = 0;
-let ticking = false;
-
-function doSomething(scroll_pos) {
+window.addEventListener('load', (event) => {
     document.getElementsByTagName('body')[0].classList.add('loaded');
-}
+ }); 
 
-window.addEventListener('scroll', function(e) {
-  last_known_scroll_position = window.scrollY;
+// let last_known_scroll_position = 0;
+// let ticking = false;
 
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething(last_known_scroll_position);
-      ticking = false;
-    });
+// function doSomething(scroll_pos) {
+//     document.getElementsByTagName('body')[0].classList.add('loaded');
+// }
 
-    ticking = true;
-  }
-});
+// window.addEventListener('scroll', function(e) {
+//   last_known_scroll_position = window.scrollY;
+
+//   if (!ticking) {
+//     window.requestAnimationFrame(function() {
+//       doSomething(last_known_scroll_position);
+//       ticking = false;
+//     });
+
+//     ticking = true;
+//   }
+// });
